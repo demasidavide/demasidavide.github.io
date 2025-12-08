2d2726e88d9beaf326aa94cd04098e29f76f7d4b
demasidavide <demasi.dav@gmail.com>
2025-12-08 21:33:44 +0100

    cv

---
+++ assets/javascript/info.js
@@ -0,0 +1,11 @@
+//https://api.open-meteo.com/v1/forecast?latitude=45.563&longitude=8.058&daily=weather_code&hourly=temperature_2m,weather_code
+const url = "https://api.open-meteo.com/v1/forecast?latitude=45.563&longitude=8.058&daily=weather_code&hourly=temperature_2m,weather_code"
+async function chiamata(){
+    const response = await fetch(url)
+    const data = await response.json();
+    console.log(data)
+    console.log("Dati giornalieri:", data.daily);
+        
+        console.log("Temp Min oggi:", data.daily,time);
+}
+chiamata()
